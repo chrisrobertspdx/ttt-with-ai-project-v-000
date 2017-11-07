ca41ba0ede1fe176f908fd478a339a1010e68df4
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-07 18:40:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -15,6 +15,9 @@ module Players
       # intermediate level
       win_combos = Game.win_combos
       theoretical_board = Board.new
+      theoretical_board = board.cells.clone
+      theoretical_board.update(open_cells.sample.to_s,self)
+      puts theoretical_board.cells.inspect
 
       s = open_cells.sample.to_s
       puts "#{self.token} picks square #{s}."
